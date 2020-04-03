434ab7930146e75f4ab235be2cb5855b5874ee0c
Andrei Orlov <aaorlov1@gmail.com>
2020-04-03 19:28:49 +0400

    update release.sh

--- Makefile
+++ Makefile
@@ -1,28 +1,10 @@
-.PHONY: build build-docker repl shell ghcid push-dms
+.PHONY: build repl shell ghcid push-dms
 
 build: update-default-nix
 	nix build nixpkgs.haskellPackages.dm -I nixpkgs=nix
 
 repl: update-default-nix
 	nix-shell --pure shell.nix --run "cabal repl lib:dm" -I nixpkgs=nix
-
-build-docker: update-default-nix update-b2b-helm-nix
-	nix build nixpkgs.dms-container \
-		--arg migrations ./migrations \
-		--arg server-cert ./tmp/server_cert.pem \
-		--arg server-key ./tmp/server_key.pem \
-		--arg passwd ./tmp/passwd \
-		--arg ssh-config ./tmp/ssh_config \
-		--arg ssh-known-hosts ./tmp/ssh_known_hosts \
-		--arg deploy-key ./tmp/deploy.key \
-		-I nixpkgs=nix \
-		-o dms-docker
-
-	nix build nixpkgs.dmc-container \
-		--arg client-cert ./tmp/client_cert.pem \
-		--arg client-key ./tmp/client_key.pem \
-		-I nixpkgs=nix \
-		-o dmc-docker
 
 update-default-nix:
 	nix-shell --pure -p cabal2nix --run "cabal2nix ." -I nixpkgs=nix > default.nix
--- release.sh
+++ release.sh
@@ -2,15 +2,133 @@
 
 set -e
 
+aws_login() {
+    aws ecr get-login-password | docker login --username AWS --password-stdin 560065381221.dkr.ecr.us-east-1.amazonaws.com
+}
+
+build_passwd() {
+    echo "root:x:0:0:root:/root:/bin/bash" > $configs/passwd
+}
+
+build_ssh_configs() {
+    echo -e "Host github.com\nHostname github.com\nPort 22\nUser git\nIdentityFile /root/.ssh/deploy.key" > $configs/ssh_config
+
+    echo "|1|wrBnhg5iUmQx07loxtzNieZp4Gs=|zZDaK51GEIn+RiA8+US9aEKn+s4= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > $configs/ssh_known_hosts
+    echo "|1|YxKPhPv5F8JFFTL6W+p46Vc1Dl4=|GgqpjQ6ViPn3oU948zb4YrdDs84= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> $configs/ssh_known_hosts
+}
+
+build_credentials() {
+    openssl req -x509 -newkey rsa:4096 \
+        -keyout $configs/server_key.pem \
+        -out $configs/server_cert.pem \
+        -nodes -subj "/CN=localhost/O=Server"
+    openssl req -newkey rsa:4096 \
+        -keyout $configs/client_key.pem \
+        -out $configs/client_csr.pem \
+        -nodes -subj "/CN=Client"
+    openssl x509 -req \
+        -in $configs/client_csr.pem \
+        -CA $configs/server_cert.pem \
+        -CAkey $configs/server_key.pem \
+        -out $configs/client_cert.pem -set_serial 01 -days $cert_expiration
+}
+
+download_deploy_key() {
+    key=`aws ssm get-parameter --name "$1" --with-decryption | jq '.Parameter.Value'`
+    echo -e $key > $configs/deploy.key
+}
+
+build_configs() {
+    build_passwd
+    build_ssh_configs
+    build_credentials
+    download_deploy_key $1
+}
+
+
+update_dependencies() {
+    make update-default-nix
+    make update-b2b-helm-nix
+}
+
+build_dmc_docker_image() {
+    nix build nixpkgs.dmc-container \
+        --arg client-cert $1/client_cert.pem \
+        --arg client-key $1/client_key.pem \
+        -I nixpkgs=nix \
+        -o $2
+}
+
+build_dms_docker_image() {
+    nix build nixpkgs.dms-container \
+        --arg migrations $2 \
+        --arg server-cert $1/server_cert.pem \
+        --arg server-key $1/server_key.pem \
+        --arg passwd $1/passwd \
+        --arg ssh-config $1/ssh_config \
+        --arg ssh-known-hosts $1/ssh_known_hosts \
+        --arg deploy-key $1/deploy.key \
+        -I nixpkgs=nix \
+        -o $3
+}
+
+push_docker_images() {
+    outfile=latest-dms-docker
+    for image_name in $dmc_docker $dms_docker; do
+        image_type=`echo $image_name | cut -d- -f1`
+        image=`ls -ls $image_name | awk '{print $12}'`
+        echo "size: $(du -sh $image)"
+        docker load --input $image | tee $outfile
+        nixcontainer=`cat $outfile | awk '{print $3}'`
+        docker tag $nixcontainer 560065381221.dkr.ecr.us-east-1.amazonaws.com/${image_type}:${tag}
+        docker tag $nixcontainer 560065381221.dkr.ecr.us-east-1.amazonaws.com/${image_type}:latest
+        docker push 560065381221.dkr.ecr.us-east-1.amazonaws.com/${image_type}:${tag}
+        echo "Published: ${image_type}:${tag}"
+    done
+    docker push 560065381221.dkr.ecr.us-east-1.amazonaws.com/dmc:latest
+    docker push 560065381221.dkr.ecr.us-east-1.amazonaws.com/dms:latest
+    echo "Published: dmc:latest"
+    echo "Published: dms:latest"
+    rm $outfile
+}
+
+key="b2b-helm-repo-user-deploy-key"
+
+case "$1" in
+    user)
+        echo "$1 mode"
+        key="b2b-helm-repo-user-deploy-key"
+        ;;
+    ci)
+        echo "$1 mode"
+        key="b2b-helm-repo-ci-deploy-key"
+        ;;
+    rundesk)
+        echo "$1 mode"
+        key="b2b-helm-repo-rundesk-deploy-key"
+        ;;
+    *)
+        echo "usage: $0 <mode>"
+        echo "mode = user | ci | rundesk"
+        exit 1
+        ;;
+esac
+
 export tag=$(git rev-parse HEAD)
-$(aws ecr get-login --no-include-email --region us-east-1)
-make build-docker
-export image=$(ls -ls dms-docker | awk '{print $12}')
-echo "size: $(du -sh $image)"
-export outfile=latest-dms-docker
-docker load --input $image | tee $outfile
-export nixcontainer=$(cat $outfile | awk '{print $3}')
-rm $outfile
-docker tag $nixcontainer 560065381221.dkr.ecr.us-east-1.amazonaws.com/default:dms-${tag}
-docker push 560065381221.dkr.ecr.us-east-1.amazonaws.com/default:dms-${tag}
-echo "Published: dms-${tag}"
+export migrations="./migrations"
+export configs=`mktemp -d`
+export dmc_docker="dmc-docker"
+export dms_docker="dms-docker"
+export cert_expiration=730
+
+echo "using '$configs' directory"
+
+aws_login
+build_configs $key
+update_dependencies
+build_dmc_docker_image $configs $dmc_docker
+build_dms_docker_image $configs $migrations $dms_docker
+push_docker_images
+
+rm -r $configs
+echo "'$configs' directory deleted"
