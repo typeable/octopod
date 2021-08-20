648e654ece40063ad2c229a2cda3c5547f15eb1c
iko <ilyakooo0@gmail.com>
2021-08-20 19:31:28 +0300

    Nixified css (#61)
    * Nixified frontend
    
    * Use single nixpkgs
    
    * wip
    
    * Remove prod folder and create www
    
    * Format
    
    * Nixified first dev scripts
    
    * Moved all caddy to nix

---
+++ dev/default.nix
@@ -0,0 +1,151 @@
+{ pkgs ? hsPkgs.pkgs
+, sources ? import ../nix/sources.nix
+, nix-filter ? import sources.nix-filter
+, hsPkgs ? import ./.. { }
+, migrations ? ../migrations
+, octopod-css ? import ../octopod-css { inherit pkgsSrc; }
+, pkgsSrc ? hsPkgs.pkgsSrc
+}:
+let frontendConfig = pkgs.writeTextDir "config.json" ''
+  {
+    "app_url": "http://localhost:8000",
+    "ws_url": "ws://localhost:4020",
+    "app_auth": "",
+    "kubernetes_dashboard_url_template": "https://echo-url.github.io/#"
+  }
+'';
+in
+{
+  frontend =
+    let
+      frontend = hsPkgs.octopod-frontend-pretty;
+
+      caddyConfig = pkgs.writeText "caddy-config" ''
+        http://localhost:8000
+
+        reverse_proxy /api/* localhost:3002
+
+        file_server /config.json {
+          root ${frontendConfig}
+        }
+
+        file_server {
+          root ${frontend}
+        }
+
+        log {
+          output stdout
+          format single_field common_log
+        }
+      '';
+
+      caddyRun = pkgs.writeScript "run-caddy.sh" ''
+        #!${pkgs.bash}/bin/bash
+
+        ${pkgs.caddy}/bin/caddy run --config ${caddyConfig} --adapter caddyfile
+      '';
+    in
+    caddyRun;
+
+  caddyForGhcid =
+    let
+      caddyConfig = pkgs.writeText "caddy-config" ''
+        http://localhost:8000
+
+        reverse_proxy /api/* localhost:3002
+
+        @notStatic {
+          not file {
+            root ${octopod-css}
+          }
+          not path /config.json
+        }
+
+        reverse_proxy @notStatic localhost:3003
+
+        file_server /config.json {
+          root ${frontendConfig}
+        }
+
+        file_server {
+          root ${octopod-css}
+        }
+
+        log {
+          output stdout
+          format single_field common_log
+        }
+      '';
+
+      caddyRun = pkgs.writeScript "run-caddy.sh" ''
+        #!${pkgs.bash}/bin/bash
+
+        ${pkgs.caddy}/bin/caddy run --config ${caddyConfig} --adapter caddyfile
+      '';
+    in
+    caddyRun;
+
+  backend =
+    let
+      echoScript = pkgs.writeScript "echo.sh" ''
+        #!${pkgs.bash}/bin/bash
+
+        echo $0 $@
+        exit 0
+      '';
+
+      failScript = pkgs.writeScript "fail.sh" ''
+        #!${pkgs.bash}/bin/bash
+
+        exit 1
+      '';
+
+      infoScript = pkgs.writeScript "info.sh" ''
+        #!${pkgs.bash}/bin/bash
+
+        echo "key,value"
+        echo "key2,value2"
+
+        exit 0
+      '';
+
+      writeScript = pkgs.writeScript "write.sh" ''
+        #!${pkgs.bash}/bin/bash
+
+        #!/bin/bash
+
+        echo $0 $@ >> ./tmp/calls.txt
+        exit 0
+      '';
+
+      runBackend = pkgs.writeScript "run-backend.sh" ''
+        #!${pkgs.bash}/bin/bash
+
+        ${pkgs.sqitchPg}/bin/sqitch deploy --chdir ${migrations} -t postgresql://octopod:octopod@localhost:5432/octopod
+
+        export PROJECT_NAME="Example Project"
+        export BASE_DOMAIN=octopod.example.com
+        export NAMESPACE=deployment
+        export ARCHIVE_RETENTION=60
+        export STATUS_UPDATE_TIMEOUT=600
+        export CREATION_COMMAND=${echoScript}
+        export UPDATE_COMMAND=${echoScript}
+        export UPDATE_ENVS_COMMAND=${echoScript}
+        export ARCHIVE_COMMAND=${echoScript}
+        export CHECKING_COMMAND=${echoScript}
+        export CLEANUP_COMMAND=${echoScript}
+        export ARCHIVE_CHECKING_COMMAND=${echoScript}
+        export TAG_CHECKING_COMMAND=${echoScript}
+        export INFO_COMMAND=${infoScript}
+        export NOTIFICATION_COMMAND=${writeScript}
+        export POWER_AUTHORIZATION_HEADER="123"
+        ${hsPkgs.octopod-backend.components.exes.octopod-exe}/bin/octopod-exe \
+            --port 4443 \
+            --ui-port 3002 \
+            --ws-port 4020 \
+            --db "host='127.0.0.1' port=5432 user='octopod' password='octopod'" \
+            --db-pool-size 10
+      '';
+    in
+    runBackend;
+}
---
+++ octopod-css/default.nix
@@ -0,0 +1,43 @@
+{ pkgsSrc ? (import ./.. { }).pkgsSrc
+, sources ? import ../nix/sources.nix
+, nix-filter ? import sources.nix-filter
+}:
+let
+  pkgs = pkgsSrc {
+    overlays = [ (self: super: { nodejs = self.nodejs-10_x; }) ];
+  };
+  production-css = pkgs.mkYarnPackage {
+    name = "octopod-css";
+    src = nix-filter {
+      root = ./.;
+      name = "octopod-css";
+      include = with nix-filter; [
+        (inDirectory ./development)
+        (inDirectory ./favicons)
+        ./gulpfile.js
+        ./package.json
+        ./.stylelintrc.json
+      ];
+    };
+    yarnLock = ./yarn.lock;
+    buildPhase = "yarn run gulp";
+    doDist = false;
+    distPhase = " ";
+    installPhase = ''
+      runHook preInstall
+
+      cp -av deps/octopod-css/production $out
+
+      runHook postInstall
+    '';
+  };
+in
+pkgs.runCommand "octopod-production-assets"
+{ } ''
+  mkdir $out
+  cp -av ${./.}/favicons/* $out
+  mkdir $out/static
+  cp -av ${production-css}/images $out/static
+  cp -av ${production-css}/styles $out/static
+  cp -av ${production-css}/vendors $out/static
+''
