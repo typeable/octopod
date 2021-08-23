097f2b59ee3cec76fa2ab06eb768948e55a7e2bf
Aleksei Sizov <a.sizov@typeable.io>
2021-08-23 16:25:59 +0300

    More informative local install script

--- octopod_local_install.sh
+++ octopod_local_install.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 set -e
+message () {
+    echo "==> $1"
+}
 for cmd in kind kubectl helm; do
    if ! command -v $cmd &> /dev/null; then
-           echo "$cmd is not installed. Please, install it!"
+           message "$cmd is not installed. Please, install it!"
            exit 1;
    fi
 done
-if [ $(helm version --short | awk -F '.' '{print $1}') != "v3" ]; then
-    echo 'Your version of helm is not supported. Please install helm 3'
+if [ "$(helm version --short | awk -F '.' '{print $1}')" != "v3" ]; then
+    message 'Your version of helm is not supported. Please install helm 3'
     exit 1
 fi
-if [ $(kind version | awk -F ' ' '{print $2}') != "v0.11.1" ]; then
-    echo "Warning! This kind version hasn't been tested"
+if [ "$(kind version | awk -F ' ' '{print $2}')" != "v0.11.1" ]; then
+    message "Warning! This kind version hasn't been tested"
 fi
 if kind get clusters | grep -q octopod; then
     echo -n 'Octopod kind cluster already exists. Do you want do delete it? [y/n] ';
     read confirm
     if [ "$confirm" != 'y' ]; then
-       echo "Exiting"
+       message "Exiting"
        exit 0
     else
-       echo "Deleting and recreating cluster"
+       message "Deleting and recreating cluster"
        kind delete cluster --name octopod
     fi
 fi
+message "Creating kind cluster"
 cat <<EOF | kind create cluster --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
@@ -75,17 +79,23 @@ data:
         loadbalance
     }
 EOF
+message "Installing ingress-nginx controller"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.45.0/deploy/static/provider/kind/deploy.yaml
 while true; do
         if [ $(kubectl -n ingress-nginx get deploy | awk -F ' ' '$1 ~ /^ingress-nginx/ {print $4}') == '1' ]; then
-                echo 'ingress-controller is ready'
+                message 'ingress-controller is ready'
                 break;
         else
-                echo 'waiting for ingress-controller to become ready...'; sleep 5;
+                message 'waiting for ingress-controller to become ready...'; sleep 5;
         fi
 done
+message "Creating nessesary namespaces"
 kubectl create ns octopod
 kubectl create ns octopod-deployment
-helm repo add --force-update typeable https://typeable.github.io/octopod
+message "Adding typeable helm repository"
+helm repo add typeable https://typeable.github.io/octopod
 helm repo update
+message "Installing octopod helm chart"
 helm -n octopod install octopod typeable/octopod --wait --timeout=5m --set octopod.baseDomain="lvh.me" --set ingress.tls.enabled=false
+message "Octopod was successfully installed! Use instruction above to connect to it."
+message "To uninstall octopod just run: kind delete cluster --name octopod"
