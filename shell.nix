0f75dc16517ca7064983e322895ff9bc29297e7e
iko <ilyakooo0@gmail.com>
2021-08-24 17:06:29 +0300

    Bumped nix dependencies (#83)
    * Bumped nix dependencies
    
    * bumped yet again

--- shell.nix
+++ shell.nix
@@ -2,10 +2,7 @@
   tools = {
     cabal = "3.2.0.0";
     hlint = "latest";
-    haskell-language-server = {
-      version = "latest";
-      index-state = "2021-07-02T00:00:00Z";
-    };
+    haskell-language-server = "latest";
     ghcid = "latest";
   };
 
