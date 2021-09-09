467d26b8545723bbf978f3a24269494d53844bea
iko <ilyakooo0@gmail.com>
2021-09-09 17:21:59 +0300

    Created Control.CacheMap

---
+++ octopod-backend/src/Control/CacheMap.hs
@@ -0,0 +1,76 @@
+module Control.CacheMap
+  ( CacheMap,
+    initCacheMap,
+    lookupBlocking,
+  )
+where
+
+import Control.Concurrent.Lifted
+import Control.Monad
+import Control.Monad.Base
+import Control.Monad.Trans.Control
+import Data.Fixed
+import Data.IORef.Lifted
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Time
+import System.Mem.Weak
+
+data CacheMap m k v = CacheMap !(InternalCacheMap k v) !(k -> m v) !NominalDiffTime
+
+type InternalCacheMap k v = IORef (Map k (UTCTime, MVar v))
+
+initCacheMap ::
+  -- | Time after which a value is considered invalid. Not exact.
+  NominalDiffTime ->
+  -- | Time after which the value should be updated.
+  -- (A value can still be valid and need updating – the caller will get the
+  -- old valid value in that case.)
+  NominalDiffTime ->
+  (k -> m v) ->
+  IO (CacheMap m k v)
+initCacheMap invalid update createValue = do
+  ref <- newIORef M.empty
+  wRef <- mkWeakIORef ref (pure ())
+  void $ fork $ removeInvalidCaches invalid wRef
+  pure $ CacheMap ref createValue update
+
+removeInvalidCaches ::
+  -- | Time after which a value is considered invalid. Not exact.
+  NominalDiffTime ->
+  Weak (InternalCacheMap k v) ->
+  IO ()
+removeInvalidCaches invalid wRef = do
+  threadDelayMicro (realToFrac $ invalid / 10)
+  cutoff <- addUTCTime (negate invalid) <$> getCurrentTime
+  deRefWeak wRef >>= \case
+    Nothing -> pure ()
+    Just ref -> do
+      atomicModifyIORef' ref $ \m -> (M.filter (\(t, _) -> t > cutoff) m, ())
+      removeInvalidCaches invalid wRef
+  where
+    threadDelayMicro :: Micro -> IO ()
+    threadDelayMicro (MkFixed i) = threadDelay (fromInteger i)
+
+lookupBlocking :: Ord k => MonadBaseControl IO m => CacheMap m k v -> k -> m v
+lookupBlocking (CacheMap mRef createValue update) k = do
+  now <- liftBase getCurrentTime
+  blank <- newEmptyMVar
+  (vM, recreate) <-
+    atomicModifyIORef'
+      mRef
+      ( \m -> case M.lookup k m of
+          Nothing -> (M.insert k (now, blank) m, (blank, True))
+          Just (timeCreated, vM)
+            | timeCreated < addUTCTime (negate update) now ->
+              (M.insert k (now, vM) m, (vM, True))
+          Just (_, vM) -> (m, (vM, False))
+      )
+
+  when recreate $
+    void $
+      fork $ do
+        v' <- createValue k
+        void $ swapMVar vM v'
+
+  readMVar vM
